4ed9694a17acb6def20e93bf7da2b5f04c4dd074
Mihai <mihaicojocaru@Mihais-MacBook-Air.local>
2025-05-07 14:23:11 +0300

    first commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Mihai Cojocaru on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
