a34a433ea51dc9d15c19aa625654f11189fcd3c8
Mihai <mihaicojocaru@Mihais-MacBook-Air.local>
2025-05-12 09:58:47 +0300

    Task

---
+++ SwiftConcurrency/Task/TaskView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskView.swift
+//  SwiftConcurrency
+//
+//  Created by Mihai Cojocaru on 10/05/2025.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskView()
+}
---
+++ SwiftConcurrency/Task/TaskViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskViewModel.swift
+//  SwiftConcurrency
+//
+//  Created by Mihai Cojocaru on 10/05/2025.
+//
+
+import Foundation
